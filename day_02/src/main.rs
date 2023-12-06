de9d2f60f498ff40b8990c550a7a15efd85dfef2
Brian Tang <ashrielbrian@gmail.com>
2023-12-06 22:29:53 +0800

    Completed part 2 of day 2

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -2,12 +2,10 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 
-/* Determine which games would have been possible if the bag had been loaded
+/* PART ONE: Determine which games would have been possible if the bag had been loaded
 with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of
  the IDs of those games? */
 fn parse_cube_line(line: &str) -> (u32, Vec<HashMap<&str, u32>>) {
-    // let cube_sets = Vec::new();
-
     let re = Regex::new(r"Game (?P<game>[0-9]+):").unwrap();
     let parsed_regex = re.captures(line).unwrap();
 
@@ -39,7 +37,7 @@ fn parse_games(line: &str) -> Vec<HashMap<&str, u32>> {
         .collect()
 }
 
-fn main() {
+fn part_one() {
     let mut requirements: HashMap<&str, u32> = HashMap::new();
     requirements.insert("red", 12);
     requirements.insert("green", 13);
@@ -65,5 +63,40 @@ fn main() {
         })
         .fold(0 as u32, |acc, val| acc + val.0);
 
-    println!("{}", game_sum);
+    println!("Part one: {}", game_sum);
+}
+
+fn part_two() {
+    let cubes_text =
+        fs::read_to_string("cubes.txt").expect("Should have been able to read cubes text file");
+
+    let game_power = cubes_text
+        .lines()
+        .map(|line| parse_cube_line(line))
+        .map(|(_, sets)| {
+            let mut red_max = &std::u32::MIN;
+            let mut green_max = &std::u32::MIN;
+            let mut blue_max = &std::u32::MIN;
+
+            for set in &sets {
+                if set.get("red").unwrap_or(&0) > red_max {
+                    red_max = set.get("red").unwrap_or(&0);
+                }
+                if set.get("green").unwrap_or(&0) > green_max {
+                    green_max = set.get("green").unwrap_or(&0);
+                }
+                if set.get("blue").unwrap_or(&0) > blue_max {
+                    blue_max = set.get("blue").unwrap_or(&0);
+                }
+            }
+
+            red_max * blue_max * green_max
+        })
+        .fold(0, |acc, val| acc + val);
+
+    println!("Part two: {}", game_power);
+}
+fn main() {
+    part_one();
+    part_two();
 }
