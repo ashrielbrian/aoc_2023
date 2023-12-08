b46f6102514298494f7151813fb3a99735d5af54
Brian Tang <ashrielbrian@gmail.com>
2023-12-08 23:52:39 +0800

    Done part 2 d3

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -2,6 +2,16 @@ use std::cmp::{Ord, Ordering};
 use std::collections::HashSet;
 use std::fs;
 
+static OFFSETS: &'static [(i32, i32)] = &[
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 fn get_digit<'a>(
     row: i32,
     col: i32,
@@ -45,28 +55,7 @@ fn get_digit<'a>(
     return "".to_string();
 }
 
-fn main() {
-    static OFFSETS: &'static [(i32, i32)] = &[
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-
-    let schematics_text =
-        fs::read_to_string("schematics.txt").expect("Should be able to read schematics text file.");
-
-    let matrix: Vec<Vec<&str>> = schematics_text
-        .lines()
-        .map(|line| line.split("").collect())
-        .collect();
-
-    println!("{} {}", matrix.len(), matrix[0].len());
-
+fn part_one(matrix: &Vec<Vec<&str>>) {
     let mut visited = HashSet::new();
     let mut total_parts = 0;
 
@@ -99,15 +88,67 @@ fn main() {
             }
         }
     }
+    println!("Part one: {}", total_parts)
+}
 
-    println!("{}", total_parts)
+fn part_two(matrix: &Vec<Vec<&str>>) {
+    let mut visited = HashSet::new();
+    let mut total_gears = 0;
 
-    // let vals = get_digit(0, 6, 0, &matrix, &mut visited);
-    // println!("{vals}");
-    // let vals = get_digit(0, 7, 0, &matrix, &mut visited);
-    // println!("{vals}");
-    // let vals = get_digit(0, 8, 0, &matrix, &mut visited);
-    // println!("{vals}");
-    // let vals = get_digit(0, 9, 0, &matrix, &mut visited);
-    // println!("{vals}");
+    for row in 0..matrix.len() {
+        for col in 0..matrix[row].len() {
+            if let Some(char) = &matrix[row][col].chars().next() {
+                if !char.is_digit(10) && *char == '*' {
+                    // use off-sets to find the numbers
+                    let adjacent_numbers: Vec<i32> = OFFSETS
+                        .iter()
+                        .map(|(row_offset, col_offset)| {
+                            let movement = match (*row_offset, *col_offset) {
+                                (0, -1) => -1,
+                                (0, 1) => 1,
+                                _ => 0,
+                            };
+
+                            get_digit(
+                                row as i32 + row_offset,
+                                col as i32 + col_offset,
+                                movement,
+                                &matrix,
+                                &mut visited,
+                            )
+                        })
+                        .filter(|digit_str| digit_str.len() > 0)
+                        .map(|digit_str| digit_str.parse::<i32>().unwrap())
+                        .collect();
+
+                    // magic number exactly 2, as given by the problem definition. gear ratio when exactly
+                    // two adjacent numbers beside a `*`
+                    if adjacent_numbers.len() == 2 {
+                        if let [first, second] = adjacent_numbers.as_slice() {
+                            total_gears += first * second;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("Part two: {}", total_gears)
+}
+
+fn main() {
+    let schematics_text =
+        fs::read_to_string("schematics.txt").expect("Should be able to read schematics text file.");
+
+    let matrix: Vec<Vec<&str>> = schematics_text
+        .lines()
+        .map(|line| line.split("").collect())
+        .collect();
+
+    println!(
+        "Matrix Input -> Rows: {}, Cols: {}",
+        matrix.len(),
+        matrix[0].len()
+    );
+    part_one(&matrix);
+    part_two(&matrix);
 }
