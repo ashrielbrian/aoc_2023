6e083088896b0ddfd6b9366687be2e10b9ab6c48
Brian Tang <ashrielbrian@gmail.com>
2023-12-10 18:06:03 +0800

    Solved p2d4

--- day_04/src/main.rs
+++ day_04/src/main.rs
@@ -22,10 +22,43 @@ fn parse_numbers(numbers: &str) -> (Vec<u32>, Vec<u32>) {
         (Vec::new(), Vec::new())
     }
 }
-fn main() {
-    let tickets = fs::read_to_string("tickets.txt").expect("Expected to read tickets file.");
 
-    let total_points: u32 = tickets
+fn part_one(tickets: &String) {
+    let total_points: u32 = get_matching_wins(tickets)
+        .iter()
+        .map(|num_wins| match num_wins.cmp(&0) {
+            Ordering::Less => 0,
+            Ordering::Equal => 0,
+            Ordering::Greater => 2_u32.pow(*num_wins as u32 - 1),
+        })
+        .fold(0, |acc, val| acc + val);
+
+    println!("Part one: {}", total_points);
+}
+
+fn part_two(tickets: &String) {
+    // think of each ticket as a bucket. each bucket can have multiple copies
+    // of itself. e.g. if the i'th ticket has x matches, then all subsequent
+    // (i + 1) to (i + x) tickets will get one additional copy and so on.
+    // so this problem is just a matter of updating the buckets.
+    let mut bucket_ticket_count: Vec<usize> = vec![1; tickets.lines().count()];
+    let matching_wins_per_ticket = get_matching_wins(tickets);
+
+    for i in 0..bucket_ticket_count.len() {
+        for j in 1..matching_wins_per_ticket[i] + 1 {
+            bucket_ticket_count[i + j] += bucket_ticket_count[i];
+        }
+    }
+
+    let total_tickets = bucket_ticket_count
+        .into_iter()
+        .fold(0, |acc, val| acc + val);
+
+    println!("Part two: {}", total_tickets);
+}
+
+fn get_matching_wins(tickets: &String) -> Vec<usize> {
+    tickets
         .lines()
         .map(|line| {
             let (winning, ticket) = parse_ticket(line);
@@ -36,15 +69,14 @@ fn main() {
                 .into_iter()
                 .filter(|num| winning.contains(num))
                 .count();
-            let ticket_points = match num_wins.cmp(&0) {
-                Ordering::Less => 0,
-                Ordering::Equal => 0,
-                Ordering::Greater => 2_u32.pow(num_wins as u32 - 1),
-            };
 
-            ticket_points
+            num_wins
         })
-        .fold(0, |acc, val| acc + val);
+        .collect()
+}
 
-    println!("{}", total_points);
+fn main() {
+    let tickets = fs::read_to_string("tickets.txt").expect("Expected to read tickets file.");
+    part_one(&tickets);
+    part_two(&tickets);
 }
